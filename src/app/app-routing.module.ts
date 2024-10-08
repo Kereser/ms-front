10669f9f4052e91a8e395b7d0ef4b6f2e39a0c13
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-10-08 13:46:53 -0500

    Modules with constants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticleListComponent } from './components/molecules/article-list/article-list.component';
 import { FormCreateComponent } from './components/molecules/form-create/form-create.component';
+import { Constants } from './utils/Constants';
 
 const routes: Routes = [
-  { path: 'create/:type', component: FormCreateComponent }, // Ruta dinámica con un parámetro
-  { path: 'dashboard', component: ArticleListComponent }, // Ruta para el Dashboard
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' } // Redirige a Dashboard si la ruta está vacía
+  { path: Constants.CREATE_PATH, component: FormCreateComponent },
+  { path: Constants.DASHBOARD_PATH, component: ArticleListComponent },
+  { path: Constants.EMPTY, redirectTo: Constants.REDIRECT_DASHBOARD_PATH, pathMatch: Constants.FULL_PATH }
 ];
 
 @NgModule({
