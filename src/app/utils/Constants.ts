1bebd76fe0e19a09fec3da985fdc3698ada852ee
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-10-17 16:04:43 -0500

    Refactor constatns and add new validators for aux-depot

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -8,30 +8,40 @@ import { CategoryModel } from "../shared/models/CategoryModel";
 export const OPTION_LIST_FOR_ADMIN_PANEL: Array<string> = ["Article", "Brand", "Category"];
 
 export const Consts = {
-	NAME: 'name',
-	NAME_CAPI: 'Name',
 	ID: 'id',
+
 	TYPE_INPUT: 'input',
+	TYPE_TEXT: 'text',
+	TYPE_DATE: 'date',
+	TYPE_PASSWORD: 'password',
 	TYPE_SELECT: 'select',
+
+	NAME: 'name',
 	DESCRIPTION: 'description',
-	DESCRIPTION_CAPI: 'Description',
+	ID_NUMBER: 'id number',
+	PHONE_NUMBER: 'phone number',
+	BIRTH_DATE: 'birth date',
+	EMAIL: 'email',
+	PASSWORD: 'password',
+
 	CATEGORY: 'category',
 	CATEGORIES: 'Categories',
 	SORT_CATEGORY_NAMES: 'category:name',
 	CATEGORY_IDS: 'categoryIds',
+	CATEGORY_NAMES: 'Category Names',
 
 	BRAND: 'Brand',
 	BRAND_ID: 'brandId',
-	
-	ARTICLE: 'Article',
 	BRAND_NAME: 'Brand Name',
+
+	ARTICLE: 'Article',
+
 	PRICE: 'price',
 	QUANTITY: 'quantity',
-	CATEGORY_NAMES: 'Category Names',
 
 	TEST: 'test',
 	CREATED: 'Created',
-	
+
 	DEFAULT_DATE: '2024-06-08',
 
 	BUTTON: 'button',
@@ -44,6 +54,11 @@ export const Consts = {
 	TEST_FIELD_CAPI: 'TestField',
 	NON_EXISTED_TYPE: 'nonExistentType',
 	TOAST_MESSAGE: 'Toas Message',
+	EMPTY: '',
+	LABEL: 'label',
+	TYPE: 'type',
+	SMALL: 'small',
+	VALID_VALUE: 'Valid Value',
 
 	BUTTON_SELECTOR: 'app-button',
 	HEADER_SELECTOR: 'app-header',
@@ -57,13 +72,7 @@ export const Consts = {
 	BY_NAMES_PATH: '/by-names',
 	BRAND_PATH: '/brands',
 	ARTICLES_PATH: '/articles',
-	
-	EMPTY: '',
-	LABEL: 'label',
-	TYPE: 'type',
-	SMALL: 'small',
-	VALID_VALUE: 'Valid Value',
-	
+
 	FALSE: false,
 	TRUE: true,
 
@@ -73,18 +82,18 @@ export const Consts = {
 
 	ZERO: 0,
 	ONE: 1,
-  TWO: 2,
-  THREE: 3,
-  FOUR: 4,
-  FIVE: 5,
-  SIX: 6,
-  SEVEN: 7,
-  EIGHT: 8,
-  NINE: 9,
-  TEN: 10,
+	TWO: 2,
+	THREE: 3,
+	FOUR: 4,
+	FIVE: 5,
+	SIX: 6,
+	SEVEN: 7,
+	EIGHT: 8,
+	NINE: 9,
+	TEN: 10,
 	FIFTY: 50,
-  NINETY: 90,
-  ONE_HUNDRED_TWENTY: 120,
+	NINETY: 90,
+	ONE_HUNDRED_TWENTY: 120,
 	THREE_THOUSAND: 3000,
 
 	ERROR_ON_CREATE_ENTITY: 'An error was found while processing createEntity',
@@ -96,6 +105,7 @@ export const Consts = {
 	CATEGORIES_REGEX: /^[a-zA-Z]{5,}(?:\s*,\s*[a-zA-Z]{5,})*$/,
 	NUMBERS_REGEX: /^\d+$/,
 	CHARACTERS_REGEX: /^\w*$/,
+	PHONE_NUMBER_REGEX: /^(?:\+?(\d){2})?\d{10}$/,
 }
 
 export enum Direcitons {
@@ -104,18 +114,19 @@ export enum Direcitons {
 }
 
 export type ValidationRules = {
-  [key: string]: ValidatorFn[];
+	[key: string]: ValidatorFn[];
 };
 
 export interface ValidationConfig {
-  brand: ValidationRules;
-  category: ValidationRules;
-  article: ValidationRules;
+	brand: ValidationRules;
+	category: ValidationRules;
+	article: ValidationRules;
+	'aux-depot': ValidationErrors;
 }
 
 const applyTest = (regex: RegExp, val: string) => {
 	if (!regex.test(val)) {
-		return { notValidFormat: true };
+		return { pattern: true };
 	}
 
 	return null;
@@ -123,9 +134,21 @@ const applyTest = (regex: RegExp, val: string) => {
 
 const validCategories = (control: AbstractControl): ValidationErrors | null => {
 	const value = control.value ? control.value.trim() : '';
-	
+
 	return applyTest(Consts.CATEGORIES_REGEX, value);
 }
+
+const isOlderValidator = (control: AbstractControl): ValidationErrors | null => {
+	if (!control.value) {
+		return null;
+	}
+
+	const today = new Date();
+	const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+	const formDate = new Date(control.value);
+
+	return formDate <= maxDate ? null : { isMinor: true };
+};
 
 export const Validations: ValidationConfig = {
 	brand: {
@@ -139,10 +162,19 @@ export const Validations: ValidationConfig = {
 	article: {
 		name: [Validators.required, Validators.minLength(Consts.THREE), Validators.maxLength(Consts.FIFTY)],
 		description: [Validators.maxLength(Consts.ONE_HUNDRED_TWENTY), Validators.minLength(Consts.FIVE), Validators.required],
-		'price': [Validators.required, Validators.pattern(Consts.BIG_DECIMAL_REGEX)],
-		'quantity': [Validators.required, Validators.pattern(Consts.NUMBERS_REGEX)],
+		price: [Validators.required, Validators.pattern(Consts.BIG_DECIMAL_REGEX)],
+		quantity: [Validators.required, Validators.pattern(Consts.NUMBERS_REGEX)],
 		'Category Names': [validCategories],
 		'Brand Name': [Validators.required, Validators.maxLength(Consts.NINETY), Validators.minLength(Consts.FIVE)]
+	},
+	'aux-depot': {
+		name: [Validators.required, Validators.minLength(Consts.THREE), Validators.maxLength(Consts.FIFTY)],
+		description: [Validators.maxLength(Consts.ONE_HUNDRED_TWENTY), Validators.minLength(Consts.FIVE), Validators.required],
+		'id number': [Validators.required, Validators.pattern(Consts.NUMBERS_REGEX)],
+		'phone number': [Validators.required, Validators.pattern(Consts.PHONE_NUMBER_REGEX)],
+		'birth date': [Validators.required, isOlderValidator],
+		email: [Validators.required, Validators.email],
+		password: [Validators.required],
 	}
 }
 
@@ -150,21 +182,30 @@ export class Constants {
 
 	static FORM_CONFIGURATIONS = new Map<string, Array<FormField>>([
 		['brand', [
-			{ name: Consts.NAME, type: Consts.TYPE_INPUT },
-			{ name: Consts.DESCRIPTION, type: Consts.TYPE_INPUT }
+			{ name: Consts.NAME, type: Consts.TYPE_TEXT },
+			{ name: Consts.DESCRIPTION, type: Consts.TYPE_TEXT }
 		]],
 		['category', [
-			{ name: Consts.NAME, type: Consts.TYPE_INPUT },
-			{ name: Consts.DESCRIPTION, type: Consts.TYPE_INPUT}
+			{ name: Consts.NAME, type: Consts.TYPE_TEXT },
+			{ name: Consts.DESCRIPTION, type: Consts.TYPE_TEXT }
 		]],
 		['article', [
-			{ name: Consts.NAME, type: Consts.TYPE_INPUT},
-			{ name: Consts.DESCRIPTION, type: Consts.TYPE_INPUT},
-			{ name: Consts.PRICE, type: Consts.TYPE_INPUT},
-			{ name: Consts.QUANTITY, type: Consts.TYPE_INPUT},
-			{ name: Consts.CATEGORY_NAMES, type: Consts.TYPE_INPUT},
-			{ name: Consts.BRAND_NAME, type: Consts.TYPE_INPUT}
-		]]
+			{ name: Consts.NAME, type: Consts.TYPE_TEXT },
+			{ name: Consts.DESCRIPTION, type: Consts.TYPE_TEXT },
+			{ name: Consts.PRICE, type: Consts.TYPE_TEXT },
+			{ name: Consts.QUANTITY, type: Consts.TYPE_TEXT },
+			{ name: Consts.CATEGORY_NAMES, type: Consts.TYPE_TEXT },
+			{ name: Consts.BRAND_NAME, type: Consts.TYPE_TEXT }
+		]],
+		['aux-depot', [
+			{ name: Consts.NAME, type: Consts.TYPE_TEXT },
+			{ name: Consts.DESCRIPTION, type: Consts.TYPE_TEXT },
+			{ name: Consts.ID_NUMBER, type: Consts.TYPE_TEXT },
+			{ name: Consts.PHONE_NUMBER, type: Consts.TYPE_TEXT },
+			{ name: Consts.BIRTH_DATE, type: Consts.TYPE_DATE },
+			{ name: Consts.EMAIL, type: Consts.TYPE_TEXT },
+			{ name: Consts.PASSWORD, type: Consts.TYPE_PASSWORD },
+		]],
 	]);
 }
 
@@ -196,29 +237,29 @@ export const brandArticle1: BrandArticleModel = {
 }
 
 export const article1: ArticleModel = {
-	id: Consts.ONE, 
-	name: Consts.NAME, 
-	description: Consts.DESCRIPTION, 
-	price: Consts.ONE_HUNDRED_TWENTY, 
-	quantity: Consts.TWO, 
-	updatedAt: new Date(Consts.DEFAULT_DATE), 
-	categories: [categoryArticle1], 
-	brand: brandArticle1 
+	id: Consts.ONE,
+	name: Consts.NAME,
+	description: Consts.DESCRIPTION,
+	price: Consts.ONE_HUNDRED_TWENTY,
+	quantity: Consts.TWO,
+	updatedAt: new Date(Consts.DEFAULT_DATE),
+	categories: [categoryArticle1],
+	brand: brandArticle1
 }
 
 export const article2: ArticleModel = {
-	id: Consts.ONE, 
-	name: Consts.NAME, 
-	description: Consts.DESCRIPTION, 
-	price: Consts.ONE_HUNDRED_TWENTY, 
-	quantity: Consts.TWO, 
-	updatedAt: new Date(Consts.DEFAULT_DATE), 
-	categories: [categoryArticle1], 
-	brand: brandArticle1 
+	id: Consts.ONE,
+	name: Consts.NAME,
+	description: Consts.DESCRIPTION,
+	price: Consts.ONE_HUNDRED_TWENTY,
+	quantity: Consts.TWO,
+	updatedAt: new Date(Consts.DEFAULT_DATE),
+	categories: [categoryArticle1],
+	brand: brandArticle1
 }
 
 export enum ToastTypes {
-  INFO = 'info',
-  SUCCESS = 'success',
-  DANGER = 'danger',
+	INFO = 'info',
+	SUCCESS = 'success',
+	DANGER = 'danger',
 }
