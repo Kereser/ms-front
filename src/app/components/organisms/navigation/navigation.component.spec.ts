9f1e08a4130fecbe79c914d4029e2fc80ad200c2
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-10-30 23:01:52 -0500

    add userService to navigation to not allow create page when not admin

--- src/app/components/organisms/navigation/navigation.component.spec.ts
+++ src/app/components/organisms/navigation/navigation.component.spec.ts
@@ -1,12 +1,18 @@
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { NavigationComponent } from './navigation.component';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Router } from '@angular/router';
 import { TestConstants, TestUtilEnums } from '../../../utils/TestConstants';
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { Consts } from '../../../utils/Constants';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserService } from '@app/shared/services/user/user.service';
 
 @Component({ template: '' })
 class DummyComponent {}
@@ -14,17 +20,25 @@ class DummyComponent {}
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
-  let router: Router;
+  let userService: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([{ path: 'create/brand', component: DummyComponent }, {path: 'dashboard/brand', component: DummyComponent}])],
-      declarations: [NavigationComponent]
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'create/brand', component: DummyComponent },
+          { path: 'dashboard/brand', component: DummyComponent },
+        ]),
+        HttpClientTestingModule,
+      ],
+      providers: [UserService],
+      declarations: [NavigationComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavigationComponent);
+    userService = TestBed.inject(UserService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -33,38 +47,62 @@ describe('NavigationComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render Create options', () => {
-    const createOptions = fixture.debugElement.queryAll(By.css('.navigation__inner-list a'));
+  it('should render "Create" options when admin logged in', () => {
+    jest.spyOn(userService, 'getRoleValue').mockReturnValue(Consts.ADMIN);
+    fixture.detectChanges();
+
+    const createOptions = fixture.debugElement.queryAll(
+      By.css('.navigation__inner-list a')
+    );
     expect(createOptions.length).toBe(Consts.SEVEN);
-    expect(createOptions[Consts.ZERO].nativeElement.textContent).toBe(TestUtilEnums.CREATE_BRAND);
-    expect(createOptions[Consts.ONE].nativeElement.textContent).toBe(TestUtilEnums.CREATE_CATEGORY);
-    expect(createOptions[Consts.TWO].nativeElement.textContent).toBe(TestUtilEnums.CREATE_ARTICLE);
+    expect(createOptions[Consts.ZERO].nativeElement.textContent).toBe(
+      TestUtilEnums.CREATE_BRAND
+    );
+    expect(createOptions[Consts.ONE].nativeElement.textContent).toBe(
+      TestUtilEnums.CREATE_CATEGORY
+    );
+    expect(createOptions[Consts.TWO].nativeElement.textContent).toBe(
+      TestUtilEnums.CREATE_ARTICLE
+    );
+  });
+
+  it('should render NOT render "Create" options when aux-depot logged', () => {
+    jest.spyOn(userService, 'getRoleValue').mockReturnValue(Consts.AUX_DEPOT);
+    fixture.detectChanges();
+
+    const createOptions = fixture.debugElement.queryAll(
+      By.css('.navigation__inner-list a')
+    );
+    expect(createOptions.length).toBe(Consts.THREE);
+    expect(createOptions[Consts.ZERO].nativeElement.textContent).toBe(
+      Consts.BRAND_DASHBOARD
+    );
+    expect(createOptions[Consts.ONE].nativeElement.textContent).toBe(
+      Consts.CATEGORY_DASHBOARD
+    );
+    expect(createOptions[Consts.TWO].nativeElement.textContent).toBe(
+      Consts.ARTICLE_DASHBOARD
+    );
   });
 
   it('should render Dashboard links', () => {
-    const dashboardLink = fixture.debugElement.queryAll(By.css('.navigation__opt'))[1].nativeElement;
-    expect(dashboardLink.textContent).toBe(TestConstants.capitalize(TestConstants.DASHBOARD_PATH));
+    const dashboardLink = fixture.debugElement.queryAll(
+      By.css('.navigation__opt')
+    )[0].nativeElement;
+    expect(dashboardLink.textContent).toBe(
+      TestConstants.capitalize(TestConstants.DASHBOARD_PATH)
+    );
   });
 
-  it('should navigate to Create Category when the link is clicked', fakeAsync(() => {
+  it('should navigate to Brand dashboard when the link is clicked', fakeAsync(() => {
     const location: Location = TestBed.inject(Location);
-    const createCategoryLink = fixture.debugElement.query(By.css('a[href="/create/brand"]')).nativeElement;
-  
-    createCategoryLink.click();
-    tick();
-    fixture.detectChanges();     
-  
-    expect(location.path()).toBe('/create/brand');
-  }));
-  
-  
-  it('should navigate to Brand dashboard when the link is clicked', fakeAsync (() => {
-    const location: Location = TestBed.inject(Location);
-    const dashboardLink = fixture.debugElement.query(By.css('a[href="/dashboard/brand"]')).nativeElement;
-    
+    const dashboardLink = fixture.debugElement.query(
+      By.css('a[href="/dashboard/brand"]')
+    ).nativeElement;
+
     dashboardLink.click();
     tick();
-    fixture.detectChanges(); 
+    fixture.detectChanges();
 
     expect(location.path()).toBe('/dashboard/brand');
   }));
