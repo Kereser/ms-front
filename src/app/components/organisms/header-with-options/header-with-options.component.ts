09f554582e250c26e3f4040c03813a5fbf4cfe22
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-10-30 23:00:58 -0500

    header with options

---
+++ src/app/components/organisms/header-with-options/header-with-options.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-with-options',
+  templateUrl: './header-with-options.component.html',
+  styleUrls: ['./header-with-options.component.scss'],
+})
+export class HeaderWithOptionsComponent {}
