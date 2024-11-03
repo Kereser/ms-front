894cf055401df8a1fe26ac308c2374f65cd630a2
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-11-03 15:13:46 -0500

    Update module imports/exports

--- src/app/components/templates/templates.module.ts
+++ src/app/components/templates/templates.module.ts
@@ -4,10 +4,11 @@ import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { OrganismsModule } from '@app/components/organisms/organisms.module';
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { AtomsModule } from '@app/components/atoms/atoms.module';
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
 
 @NgModule({
-  declarations: [MainLayoutComponent, AuthLayoutComponent],
+  declarations: [MainLayoutComponent, AuthLayoutComponent, HomeLayoutComponent],
   imports: [CommonModule, AtomsModule, OrganismsModule],
-  exports: [MainLayoutComponent, AuthLayoutComponent],
+  exports: [MainLayoutComponent, AuthLayoutComponent, HomeLayoutComponent],
 })
 export class TemplatesModule {}
