0f50e2de1e1d362f6afe083739064bd4e2884f03
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-11-05 15:09:51 -0500

    Service to get articles for cart and delete item from it

--- src/app/shared/services/cart/cart.service.ts
+++ src/app/shared/services/cart/cart.service.ts
@@ -2,21 +2,48 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { Consts, Direcitons } from '@app/utils/Constants';
+import { PageDTO } from '@app/shared/models/PageDTO';
+import { CartPageable } from '@app/shared/models/CartPageable';
+import { CartArticleModel } from '@app/shared/models/CartArticleModel';
 
 export type ArticleCart = {
   articleId: number;
   quantity: number;
 };
 
+export interface PageCartDTO<T> extends PageDTO<T> {
+  cart: CartPageable;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   private baseURL = environment.CART_BASE_URL;
+  private getArticlesURL =
+    this.baseURL + Consts.USER_PATH + Consts.ARTICLES_PATH;
 
   constructor(private http: HttpClient) {}
 
   addArticleToCart(entity: ArticleCart): Observable<unknown> {
     return this.http.put(this.baseURL, { items: [entity] });
+  }
+
+  deleteArticle(cartId: number, articleId: number) {
+    return this.http.delete(
+      `${this.baseURL}/${cartId}${Consts.ARTICLES_PATH}/${articleId}`
+    );
+  }
+
+  getArticlesForUser(): Observable<PageCartDTO<CartArticleModel>> {
+    return this.http.get<PageCartDTO<CartArticleModel>>(this.getArticlesURL, {
+      params: {
+        page: 0,
+        pageSize: 10,
+        column: 'ksadjkfl',
+        columns: Direcitons.ASC,
+      },
+    });
   }
 }
