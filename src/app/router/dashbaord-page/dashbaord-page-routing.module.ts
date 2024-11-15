9c91344786b262bfe3cd4341cd2f40e2d0a16532
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-11-14 19:16:25 -0500

    guard home, dashboard and cart pages

--- src/app/router/dashbaord-page/dashbaord-page-routing.module.ts
+++ src/app/router/dashbaord-page/dashbaord-page-routing.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from '@app/components/pages/admin/dashboard-page/dashboard-page.component';
+import { RolesGuard } from '@app/shared/guards/roles.guard';
 import { Consts } from '@app/utils/Constants';
 
 const routes: Routes = [
   {
     path: Consts.CATEGORY,
+    canActivate: [RolesGuard],
     component: DashboardPageComponent,
-    data: { type: Consts.CATEGORY },
+    data: { type: Consts.CATEGORY, roles: [Consts.ANY_ROLE] },
   },
   {
     path: Consts.BRAND,
+    canActivate: [RolesGuard],
     component: DashboardPageComponent,
-    data: { type: Consts.BRAND },
+    data: { type: Consts.BRAND, roles: [Consts.ANY_ROLE] },
   },
   {
     path: Consts.ARTICLE,
+    canActivate: [RolesGuard],
     component: DashboardPageComponent,
-    data: { type: Consts.ARTICLE },
+    data: { type: Consts.ARTICLE, roles: [Consts.ANY_ROLE] },
   },
 ];
 
--- src/app/router/home/home-routing.module.ts
+++ src/app/router/home/home-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from '@app/components/pages/home-page/home-page.component';
+import { RolesGuard } from '@app/shared/guards/roles.guard';
 import { Consts } from '@app/utils/Constants';
 
 const routes: Routes = [
   {
     path: Consts.EMPTY,
+    canActivate: [RolesGuard],
     component: HomePageComponent,
+    data: { roles: [Consts.CLIENT] },
   },
 ];
 
